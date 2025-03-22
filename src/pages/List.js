393a3f78d8c439563b8560be7c196b67103638bc
takayukey009 <takayuki.togawa@gate-agency.com>
2025-03-22 20:08:26 +0900

    Fix: useEffect import from react instead of react-router-dom

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,4 +1,5 @@
-import { useParams, useNavigate, useEffect } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { ShoppingProvider } from '../contexts/ShoppingContext';
 import Header from '../components/Header';
 import StoreSelector from '../components/StoreSelector';
